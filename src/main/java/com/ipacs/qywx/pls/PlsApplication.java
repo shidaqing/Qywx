d5d76145d06ba89b6244caa191f95c509e09bf8d
Administrator <Administrator@PC--20171203BMT>
2018-12-07 00:35:14 +0800

    继承mybatis

--- src/main/java/com/ipacs/qywx/pls/PlsApplication.java
+++ src/main/java/com/ipacs/qywx/pls/PlsApplication.java
@@ -1,8 +1,11 @@
 package com.ipacs.qywx.pls;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
+@MapperScan(basePackages = "com.ipacs.qbook.dao")
+@ComponentScan(basePackages = {"com.ipacs.qbook"})
 @SpringBootApplication
 public class PlsApplication {
 
